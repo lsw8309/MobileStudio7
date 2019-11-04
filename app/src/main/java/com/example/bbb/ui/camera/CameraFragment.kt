0642fa26709bb60c8e5694ac03465dcc70d8b13b
lsw8309 <smy02244@naver.com>
2019-11-05 08:33:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/bbb/ui/camera/CameraFragment.kt
@@ -0,0 +1,31 @@
+package com.example.bbb.ui.camera
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.bbb.R
+
+class CameraFragment : Fragment() {
+
+    private lateinit var cameraViewModel: CameraViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        cameraViewModel =
+            ViewModelProviders.of(this).get(CameraViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_camera, container, false)
+        val textView: TextView = root.findViewById(R.id.text_camera)
+        cameraViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
